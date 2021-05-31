46628e853e31551ee6980249a93f8a03c94d43d7
Hilary Richardson <hilary.richardson@gmail.com>
2021-05-31 11:29:33 -0700

    fixing bugs in resolvers and typeDefs to sync better

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,31 +6,32 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                    .select('-__v -password')
-
+                const userData = await User.findOne({ _id: context.user._id }).select(
+                    "-__v -password"
+                );
                 return userData;
             }
-            throw new AuthenticationError('Not logged in');
-        }
+            throw new AuthenticationError("Not logged in");
+        },
     },
     Mutation: {
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
-            const token = signToken(user);
-            return { token, user };
-        },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
             if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError("Invalid credentials");
             }
 
-            const correctPw = await user.isCorrectPassword(password);
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
+            const correctPassword = await user.isCorrectPassword(password);
+            if (!correctPassword) {
+                throw new AuthenticationError("Invalid credentials");
             }
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
             const token = signToken(user);
 
             return { token, user };
@@ -40,13 +41,12 @@ const resolvers = {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
                     { $addToSet: { savedBooks: input } },
-                    { new: true }
+                    { new: true, runValidators: true }
                 );
                 return updatedUser;
             }
             throw new AuthenticationError("You need to be logged in!");
         },
-
         removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
@@ -58,8 +58,7 @@ const resolvers = {
             }
             throw new AuthenticationError("You need to be logged in!");
         },
-    }
-
+    },
 };
 
 module.exports = resolvers
